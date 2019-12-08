14614dfb88ca6db9c495a900bd70bca8591cee4f
alexbrown <ajb2969@rit.edu>
2019-12-08 14:47:16 -0500

    updated controller for printing doc-names for qrel files

--- src/main/java/retrieval/LDA.java
+++ src/main/java/retrieval/LDA.java
@@ -1,11 +1,7 @@
 package retrieval;
 import java.util.ArrayList;
 
-import org.apache.spark.ml.clustering.BisectingKMeans;
-import org.apache.spark.ml.clustering.BisectingKMeansModel;
-import org.apache.spark.ml.linalg.Vector;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
+
 
 public class LDA extends Models {
 
