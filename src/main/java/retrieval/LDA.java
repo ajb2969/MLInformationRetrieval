e03a98f73deef68bc3a652f2bf81ce56916eb083
alexbrown <ajb2969@rit.edu>
2019-12-08 12:16:05 -0500

    added design file for git lfs

--- src/main/java/retrieval/LDA.java
+++ src/main/java/retrieval/LDA.java
@@ -1,6 +1,12 @@
 package retrieval;
 import java.util.ArrayList;
 
+import org.apache.spark.ml.clustering.BisectingKMeans;
+import org.apache.spark.ml.clustering.BisectingKMeansModel;
+import org.apache.spark.ml.linalg.Vector;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
 public class LDA extends Models {
 
     public LDA() {
