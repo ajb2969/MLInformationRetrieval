c91560c17a4313aaa38e1d44b8e1921b78ad5966
alexbrown <ajb2969@rit.edu>
2019-12-09 14:57:06 -0500

    wrote parallel code to compute vector space sparse matrix in CSC format

--- src/main/java/retrieval/LDA.java
+++ src/main/java/retrieval/LDA.java
@@ -4,14 +4,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Set;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class LDA extends Models {
-
+    final String vecModelFilePath = "indicies/vecSpaceModel.tsv";
     public LDA() {
         super();
     }
 
+
+    
     static class Index {
         private int index;
         private double value;
@@ -20,29 +27,78 @@ public class LDA extends Models {
             this.index = index;
             this.value = value;
         }
+
+        /**
+         * @return the index
+         */
+        public int getIndex() {
+            return index;
+        }
+
+        /**
+         * @return the value
+         */
+        public double getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return this.index + "," + this.value;
+        }
     }
 
     public ArrayList<Similarity> retrieve(String query) {
         System.out.println("Retrieving LDA");
         String[] terms = extractTerms(query);
-        Set<String> documents = Models.getAllDocuments();
+        ConcurrentHashMap<String, List<Index>> parsedDocs;
+        File outputFile = new File(vecModelFilePath);
+        if(!outputFile.exists()) {
+            Set<String> documents = Models.getAllDocuments();
+            System.out.println("There are " + documents.size() + " documents");
+            List<String> docTokens = new ArrayList<>(Models.getTokenToEntryIndex().keySet());
+            Collections.sort(docTokens);
+            System.out.println("There are " + docTokens.size() + " tokens");
+        
+            try {
+                Scheduler schedule = new Scheduler(documents, docTokens, outputFile);
+                parsedDocs = schedule.executeParse();
+                System.out.println("The hashmap has a size of " + parsedDocs.keySet().size());
+                System.out.println("Finished lda");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+        } else {
+            parsedDocs = new ConcurrentHashMap<>();
+            System.out.println("Reading in compressed sparse matrix");
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(outputFile));
+                String line;
+                while((line = reader.readLine()) != null) {
+                    String [] entries = line.split("\t");
+                    String key = entries[0];
+                    ArrayList<LDA.Index> values = new ArrayList<>();
+                    for(int index = 1; index < entries.length; index++) {
+                        String [] column = entries[index].split(",");
+                        values.add(new LDA.Index(Integer.parseInt(column[0]), Double.parseDouble(column[1])));
+                    }
+                    parsedDocs.put(key, values);
+                }
+                reader.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("finished parsing vector space index");
 
-        System.out.println("There are " + documents.size() + " documents");
-        List<String> docTokens = new ArrayList<>(Models.getTokenToEntryIndex().keySet());
-        Collections.sort(docTokens);
-        System.out.println("There are " + docTokens.size() + " tokens");
-
-        Scheduler schedule = new Scheduler(documents, docTokens);
-        ConcurrentHashMap<String, ArrayList<Index>> parsedDocs;
-
-        try {
-            parsedDocs = schedule.executeParse();
-            System.out.println("The hashmap has a size of " + parsedDocs.keySet().size());
-            System.out.println("Finished lda");
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            
+            //read in file into hashmap
         }
+
         
             //DoubleVector tfidfTerms = new DoubleVector();
 
--- src/main/java/retrieval/Scheduler.java
+++ src/main/java/retrieval/Scheduler.java
@@ -5,8 +5,15 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 
@@ -15,50 +22,83 @@ import org.apache.tomcat.jni.Time;
 class Scheduler {
     private ExecutorService threadpool;
     private static Set<String> documents;
-    private static ConcurrentHashMap<String, ArrayList<LDA.Index>> vectorSpace;
+    private static ConcurrentHashMap<String, List<LDA.Index>> vectorSpace;
     private List<String> docTokens;
     private static CountDownLatch counter;
+    private static BufferedWriter writer;
 
-    Scheduler(Set<String> documents, List<String> docTokens) {
+    Scheduler(Set<String> documents, List<String> docTokens, File file) throws IOException {
         this.threadpool = new ThreadPoolExecutor(32, 64, Time.APR_MSEC_PER_USEC, TimeUnit.SECONDS,
                 new LinkedBlockingDeque<>());
         Scheduler.documents = documents;
         this.docTokens = docTokens;
         Scheduler.counter = new CountDownLatch(documents.size());
         vectorSpace = new ConcurrentHashMap<>();
+        Scheduler.writer = new BufferedWriter(new FileWriter(file));
     }
 
-    ConcurrentHashMap<String, ArrayList<LDA.Index>> executeParse() throws InterruptedException {
-
+    ConcurrentHashMap<String, List<LDA.Index>> executeParse() throws InterruptedException {
+        //Processed documents in parallel and stores each row with the document -> a row in CSR format
+        AtomicInteger counter = new AtomicInteger(0);
         documents.parallelStream().forEach(document -> {
-            this.threadpool.submit(new DocumentParser(document, this.docTokens));
-        });
-
+            this.threadpool.submit(new DocumentParser(document, this.docTokens, counter.addAndGet(1)));
+        }); 
         Scheduler.counter.await();
+
+        try {
+            System.out.println("Writing matrix to file");
+            for(String document: Scheduler.vectorSpace.keySet()) {
+                List<LDA.Index> tfidfValues = Scheduler.vectorSpace.get(document);
+                Scheduler.writer.write(document + "\t");
+                for(int i = 0; i < tfidfValues.size(); i++) {
+                    Scheduler.writer.write(tfidfValues.get(i).toString() + "\t");
+                }
+                Scheduler.writer.write("\n");
+                Scheduler.writer.flush();
+            }
+            System.out.println("Done writing");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return Scheduler.vectorSpace;
     }
 
     private class DocumentParser implements Runnable {
         private String document;
         private List<String> docTokens;
+        private int num;
 
-        DocumentParser(String document, List<String> docTokens) {
+        DocumentParser(String document, List<String> docTokens, int number) {
             this.document = document;
             this.docTokens = docTokens;
+            this.num = number;
         }
 
         @Override
         public void run() {
-            ArrayList<LDA.Index> columns = new ArrayList<>();
-            for (int i = 0; i < docTokens.size(); i++) {
-                double value = Models.tfidf(docTokens.get(i), document);
-                if (value != 0) {
-                    columns.add(new LDA.Index(i, value));
+            if(this.num % 1000 == 0) {
+                synchronized(System.out) {
+                    System.out.println("Starting process " + this.num);
                 }
             }
+            List<LDA.Index> columns = IntStream.range(0, this.docTokens.size())
+                .parallel()
+                .mapToObj(index -> computeTFIDF(index))
+                .filter(value -> value.isPresent())
+                .map(value -> value.get())
+                .collect(Collectors.toList());
             Scheduler.vectorSpace.put(document, columns);
             Scheduler.counter.countDown();
         }
 
+        Optional<LDA.Index> computeTFIDF(int index) {
+            double tfidfValue = Models.tfidf(docTokens.get(index), document);
+            if(tfidfValue == 0) {
+                return Optional.empty();
+            } else {
+                return Optional.of(new LDA.Index(index, tfidfValue));
+            }
+        }
     }
 }
